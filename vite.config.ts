48ac2ac51657d0127fb14bb9c08662461179d077
Steven Casteel <stevencasteel@protonmail.com>
2026-05-24 18:52:57 -0500

    build: configure relative paths for boxbattle

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BOX-BATTLE/",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
